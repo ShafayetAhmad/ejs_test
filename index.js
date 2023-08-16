3cbc27c9d33b057ac65abf257df77c8b186abdc9
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-08-16 20:31:13 +0530

    ul added

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const app = express();
 let day = new Date().getDay();
 let today = "";
 let advice = "";
+let tasks = "";
 if (day == 6 || day == 0) today = "weekend";
 else today = "weekday";
 
-if (today == "weekend") advice = "Enjoy the weekend!";
-else advice = "Work Hard!";
+if (today == "weekend") {
+    advice = "Enjoy the weekend!";
+    tasks = ["Go to the gym", "Do the laundry", "Buy groceries"];
+} 
+else {
+    advice = "Work Hard!";
+    tasks = ["Finish the lab", "Read 20 pages of the book", "Do the homework"];
+    
+}
+
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {dayType: today, advice: advice});
+    res.render("index.ejs", {dayType: today, advice: advice, tasks: tasks});
 })
 
 
